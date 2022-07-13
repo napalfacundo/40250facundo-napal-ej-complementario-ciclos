6d96660c047e28c8a65ab911e1fe9cce28149862
napalfacundo <facundosancheznapal@hotmail.com>
2022-07-12 22:10:22 -0300

    sumando arrow functions

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let cantTareas = Number(prompt("Cantidad de tareas:"));
 let listaTareas = "Lista Tareas:\n"
 
-for (i=0; i<=cantTareas-1; i++) {
-    let tituloTarea = prompt(`Ingrese titulo de la tarea numero ${i+1}`);
-    listaTareas += `Tarea nº ${i+1}:  ${tituloTarea}\n`;
+let init = () => {
+    for (i=0; i<=cantTareas-1; i++) {
+        let tituloTarea = prompt(`Ingrese titulo de la tarea numero ${i+1}`);
+        listaTareas += `Tarea nº ${i+1}:  ${tituloTarea}\n`;
+    }
+    alert(listaTareas);
 }
 
-console.log(listaTareas);
+init();
+
